6842cad0b0d533d6bea7f3c33e9a5387b67c3e18
bubibubi <u.ballestrazzi@gmail.com>
2017-06-27 19:13:26 +0200

    Added Unicode (japanese) and Skip-Take test

---
+++ JetEntityFrameworkProvider.Test/Model56_SkipTake/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JetEntityFrameworkProvider.Test.Model56_SkipTake
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Description { get; set; }
+        public DateTime? Date { get; set; }
+        public double? Value { get; set; }
+    }
+
+}
---
+++ JetEntityFrameworkProvider.Test/Model56_SkipTake/Test.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JetEntityFrameworkProvider.Test.Model56_SkipTake
+{
+    public abstract class Test
+    {
+        protected abstract DbConnection GetConnection();
+
+        [TestMethod]
+        public void SkipTakeDate()
+        {
+            using (var context = new Context(GetConnection()))
+            {
+                for (int i = 0; i < 30; i++)
+                    context.Entities.Add(new Entity() { Description = i.ToString(), Date = new DateTime(1969, 09, 15).AddDays(i % 2 == 0 ? i * 30 : - i * 30)});
+
+                context.SaveChanges();
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                foreach (Entity entity in context.Entities.ToList())
+                    Console.WriteLine(entity.Description);
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                var entities = context.Entities.OrderBy(_ => _.Date).Skip(10).Take(5).ToList();
+                Assert.AreEqual(5, entities.Count);
+                for (int i = 0; i < entities.Count - 1; i++)
+                {
+                    Entity entity = entities[i];
+                    Assert.IsTrue(entity.Date < entities[i + 1].Date);
+                }
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                context.Entities.RemoveRange(context.Entities.ToList());
+                context.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void SkipTakeString()
+        {
+            using (var context = new Context(GetConnection()))
+            {
+                for (int i = 0; i < 30; i++)
+                    context.Entities.Add(new Entity() { Description = i % 2 == 0 ? i.ToString(): (-i).ToString() });
+
+                context.SaveChanges();
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                foreach (Entity entity in context.Entities.ToList())
+                    Console.WriteLine(entity.Description);
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                var entities = context.Entities.OrderBy(_ => _.Description).Skip(10).Take(5).ToList();
+                Assert.AreEqual(5, entities.Count);
+                for (int i = 0; i < entities.Count - 1; i++)
+                {
+                    Entity entity = entities[i];
+                    Assert.AreEqual(-1, String.Compare(entity.Description , entities[i + 1].Description));
+                }
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                context.Entities.RemoveRange(context.Entities.ToList());
+                context.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void SkipTakeDouble()
+        {
+            using (var context = new Context(GetConnection()))
+            {
+                for (int i = 0; i < 30; i++)
+                    context.Entities.Add(new Entity() { Value = i % 2 == 0 ? i : -i });
+
+                context.SaveChanges();
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                foreach (Entity entity in context.Entities.ToList())
+                    Console.WriteLine(entity.Value);
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                var entities = context.Entities.OrderBy(_ => _.Value).Skip(10).Take(5).ToList();
+                Assert.AreEqual(5, entities.Count);
+                for (int i = 0; i < entities.Count - 1; i++)
+                {
+                    Entity entity = entities[i];
+                    Assert.IsTrue(entity.Value < entities[i + 1].Value);
+                }
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                context.Entities.RemoveRange(context.Entities.ToList());
+                context.SaveChanges();
+            }
+        }
+    }
+}
