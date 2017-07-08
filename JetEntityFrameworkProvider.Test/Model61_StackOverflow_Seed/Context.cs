d8187ac45c25d2ce310dec2201e4fdca9b1d8634
bubibubi <u.ballestrazzi@gmail.com>
2017-07-08 11:22:31 +0200

    Added Seed (AddOrUpdate) test

---
+++ JetEntityFrameworkProvider.Test/Model61_StackOverflow_Seed/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity;
+
+// https://stackoverflow.com/questions/44972714/ef-seed-table-with-foreign-key
+
+namespace JetEntityFrameworkProvider.Test.Model61_StackOverflow_Seed
+{
+    public class Context : DbContext
+    {
+        public Context()
+        { }
+
+
+        public Context(DbConnection connection)
+            : base(connection, false)
+        { }
+
+        public DbSet<ClassRoom> ClassRooms { get; set; }
+        public DbSet<ClassSchedule> ClassSchedules { get; set; }
+
+    }
+
+}
