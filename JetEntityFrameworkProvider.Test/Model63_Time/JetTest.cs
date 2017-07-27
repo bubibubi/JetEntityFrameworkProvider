167eadacc4a93b1cb7f504a98855819daa99d7a8
bubibubi <u.ballestrazzi@gmail.com>
2017-07-27 21:23:00 +0200

    Renamed test

--- JetEntityFrameworkProvider.Test/Model63_Time/JetTest.cs
+++ JetEntityFrameworkProvider.Test/Model63_Time/JetTest.cs
@@ -5,7 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace JetEntityFrameworkProvider.Test.Model63_Time
 {
     [TestClass]
-    public class Model62_InnerQueryBug_JetTest : Test
+    public class Model63_Time_JetTest : Test
     {
         protected override DbConnection GetConnection()
         {
