4451c0fe4724d6a2473221ecc7da150f6e86fcb2
bubibubi <u.ballestrazzi@gmail.com>
2017-07-28 16:48:51 +0200

    Added test for object caching

---
+++ JetEntityFrameworkProvider.Test/Model65_InMemoryObjects/Test.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JetEntityFrameworkProvider.Test.Model65_InMemoryObjects
+{
+    public abstract class Test
+    {
+        protected abstract DbConnection GetConnection();
+
+        [TestMethod]
+        public void Run()
+        {
+
+            Item item;
+
+
+            // Seed =============================
+            using (var context = new Context(GetConnection()))
+            {
+                context.Items.AddRange(
+                    new[]
+                    {
+                        item = new Item() {Description = "Description1"},
+                        new Item() {Description = "Description2"},
+                        new Item() {Description = "Description3"},
+                        new Item() {Description = "Description4"},
+                    });
+                context.SaveChanges();
+            }
+
+            // Upsert (object without a proxy) =============================
+            item.Description = "DescriptionChanged";
+            Upsert(item);
+
+            // Upsert (new) =============================
+            Upsert(new Item() {Description = "Item from upsert"});
+
+            using (var context = new Context(GetConnection()))
+            {
+                item = context.Items.First(_ => _.Description == "Description2");
+            }
+
+            // Upsert (object with a proxy) =============================
+            Assert.AreNotEqual(item.GetType(), typeof(Item));
+            item.Description = "Description changed to object with proxy";
+            Upsert(item);
+
+            using (var context = new Context(GetConnection()))
+            {
+                Assert.IsNull(context.Items.FirstOrDefault(_ => _.Description == "Description2"));
+            }
+
+        }
+
+        public void Upsert(Item item)
+        {
+            using (var context = new Context(GetConnection()))
+            {
+                if (item.Id == 0)
+                {
+                    context.Items.Add(item);
+                }
+                else
+                {
+                    context.Entry(item).State = EntityState.Modified;
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
