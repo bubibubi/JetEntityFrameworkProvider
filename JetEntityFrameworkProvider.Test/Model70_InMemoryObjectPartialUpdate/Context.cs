07e08326a93e5c71dc98cef308752daca7faaaca
bubibubi <u.ballestrazzi@gmail.com>
2017-08-08 17:04:38 +0200

    Added some tests

---
+++ JetEntityFrameworkProvider.Test/Model70_InMemoryObjectPartialUpdate/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity;
+
+namespace JetEntityFrameworkProvider.Test.Model70_InMemoryObjectPartialUpdate
+{
+    public class Context : DbContext
+    {
+        public Context()
+        { }
+
+
+        public Context(DbConnection connection)
+            : base(connection, true)
+        { }
+
+        public DbSet<Item> Items { get; set; }
+
+    }
+
+}
---
+++ JetEntityFrameworkProvider.Test/Model70_InMemoryObjectPartialUpdate/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JetEntityFrameworkProvider.Test.Model70_InMemoryObjectPartialUpdate
+{
+    public abstract class Test
+    {
+        protected abstract DbConnection GetConnection();
+
+        [TestMethod]
+        public void Run()
+        {
+            int itemId;
+
+            using (var context = new Context(GetConnection()))
+            {
+                Item item = new Item() {ColumnA = "ColumnA", ColumnB = "ColumnB"};
+                context.Items.Add(item);
+                context.SaveChanges();
+                itemId = item.Id;
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                Item item = new Item() { Id = itemId, ColumnA = "ColumnAUpdated"};
+                context.Entry(item).State = EntityState.Modified;
+                context.Entry(item).Property(_ => _.ColumnB).IsModified = false;
+                context.SaveChanges();
+            }
+
+            using (var context = new Context(GetConnection()))
+            {
+                Item item = context.Items.Find(itemId);
+                Assert.IsNotNull(item);
+                Assert.AreEqual("ColumnAUpdated", item.ColumnA);
+                Assert.AreEqual("ColumnB", item.ColumnB);
+            }
+
+
+
+        }
+    }
+}
