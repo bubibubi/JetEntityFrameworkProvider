9ac1b0faeeb92494a28d434040b59cd729902c61
bubibubi <u.ballestrazzi@gmail.com>
2017-09-19 16:49:58 +0200

    Moved global assemblies informations

---
+++ Build/GlobalAssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Bubi")]
+[assembly: AssemblyProduct("JetEntityFrameworkProvider")]
+[assembly: AssemblyCopyright("Copyright © 2017 - Bubi")]
+[assembly: AssemblyTrademark("Before use any of the trademarks, check if they are registered")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e77112b7-b6ba-43a8-b21f-dd79a7a47a2f")]
+
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyInformationalVersion("6.1.3-beta1")]
